70f384fb1ddd5976d23278552c837e3a27531b85
SAHAN20002 <sahan.waruna1234@gmail.com>
2025-01-10 20:03:41 +0530

    Add findCustomer function and route to fetch customers

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -19,4 +19,17 @@ const createCustomer = async (req,resp) =>{
   }
 }
 
-module.exports = {createCustomer};
+const findCustomer = async (req,resp) =>{
+  try{
+    const{searchText='',page=0,size=10} = req.query;
+    const serach = searchText ?{name:{$regex:searchText,$options:'i'}}:{};
+    const customers = await customerModel.find(serach).skip(page*size).limit(size);
+    const count = await customerModel.countDocuments(serach);
+    resp.status(200).json({message:'Customers fetched successfully',datalist:customers,datacount:count});
+  } catch(error){
+    console.log(error);
+    resp.status(500).json({message:'Internal server error'});
+  }
+}
+
+module.exports = {createCustomer,findCustomer};
--- routers/customerRouters.js
+++ routers/customerRouters.js
@@ -3,5 +3,6 @@ const CustomerRouter = express.Router();
 const customerController = require('../controllers/customerController');
 
 CustomerRouter.post('/create',customerController.createCustomer);
+CustomerRouter.get('/find',customerController.findCustomer);
 
 module.exports = CustomerRouter;
