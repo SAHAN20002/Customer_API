85a30a37630db3138d79bded85f354d6db3daf27
SAHAN20002 <sahan.waruna1234@gmail.com>
2025-01-12 23:37:03 +0530

    Refactor createOrder function for improved readability and consistency

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,37 +1,34 @@
+const orderModel = require("../model/order");
+const productModel = require("../model/product");
 
-const orderModel = require('../model/order');
-const productModel = require('../model/product');
+const createOrder = async (req, resp) => {
+  try {
+    const { customerID, products } = req.body;
 
-const createOrder = async (req,resp) =>{
-    try{
-      const {customerID,products} = req.body;
-      
-      if(!customerID || !Array.isArray(products) || products.length === 0){
-           return resp.status(400).json({message:'customerId and products are required'});
-      }
-      let totalAmount = 0;
-
-      for( const items of products){
-          const productfind = await productModel.findById(items.productId);
-          
-            if(!productfind){
-                return resp.status(400).json({message:'Product not found'});
-            }
-           
-            
-            totalAmount += productfind.price * items.quantity;
-            
-
-      }
-        const orderObject = new orderModel({customerID,products,totalAmount});
-        await orderObject.save();
-
-        resp.status(201).json({message:'Order created successfully'});
-
-    }catch(error){
-        console.log(error);
-        resp.status(500).json({message:'Internal server error',error});
+    if (!customerID || !Array.isArray(products) || products.length === 0) {
+      return resp
+        .status(400)
+        .json({ message: "customerId and products are required" });
     }
+    let totalAmount = 0;
+
+    for (const items of products) {
+      const productfind = await productModel.findById(items.productId);
+
+      if (!productfind) {
+        return resp.status(400).json({ message: "Product not found" });
+      }
+
+      totalAmount += productfind.price * items.quantity;
+    }
+    const orderObject = new orderModel({ customerID, products, totalAmount });
+    await orderObject.save();
+
+    resp.status(201).json({ message: "Order created successfully" });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({ message: "Internal server error", error });
+  }
 };
 
-module.exports = {createOrder};
+module.exports = { createOrder };
